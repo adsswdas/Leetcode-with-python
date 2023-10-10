a486cdde5c4d1c845c9b346a52e026f5e4a8725b
Shovit <119912892+Virtual4087@users.noreply.github.com>
2023-10-10 14:12:10 +0545

    Update Leetcode26.py

--- Leetcode26.py
+++ Leetcode26.py
@@ -1,9 +1,8 @@
 class Solution(object):
     def removeDuplicates(self, nums):
-        for num in nums:
-            for num1 in nums[nums.index(num)+1:]:
-                if num == num1:
-                    nums.remove(num)
-                else:
-                    break
-        return len(nums)
+        index = 1   
+        for i in range(1, len(nums)):
+            if nums[i-1] != nums[i]:
+                nums[index] = nums[i]
+                index += 1
+        return index
