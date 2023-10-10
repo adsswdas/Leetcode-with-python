e0178e7330dcd486e2c8bcead95e18c8120583cf
Shovit <119912892+Virtual4087@users.noreply.github.com>
2023-10-10 14:37:05 +0545

    Create Leetcode28.py

---
+++ Leetcode28.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        l = len(needle)
+        for i in range(len(haystack)-(l-1)):
+            if haystack[i:i+l] == needle:
+                return i
+        return -1
